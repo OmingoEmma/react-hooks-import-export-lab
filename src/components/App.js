722d5017d1ba265a8c4c3750a815dd9323de5761
OmingoEmma <emmaomingo@gmail.com>
2024-04-30 03:32:36 +0300

    Complete Lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,7 @@
 import React from "react";
+import NavBar from "../components/NavBar"; // Import NavBar component
+import Home from "../components/Home"; // Import Home component
+import About from "../components/About"; // Import About component
 
 function App() {
   return (
